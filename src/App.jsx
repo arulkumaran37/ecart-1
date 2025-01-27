fcac90af160840674ad82820ad67ace9adfb9c1d
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-01-27 20:52:19 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/layout/header';
 import { StoreProvider } from './context/store_context';
-import Heom from './pages/home/home';
 import ProductList from './pages/products/products';
 import Cart from './pages/cart/cart';
 import Profile from './pages/profile/profile';
 import './styles/global.css';
+import Home from './pages/home/home';
 
 const App = () => {
   return (
@@ -17,7 +17,6 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/products" element={<ProductList />} />
-            <Route path="/wishlist" element={<Wishlist />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/profile" element={<Profile />} />
           </Routes>
